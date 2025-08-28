027659afc4add469e2ec7be13829d5d4fddc894f
Olivier Laurent <olivier.ar.laurent@gmail.com>
2025-08-28 07:48:48 +0200

    :sparkles: Properly set starting params

--- src/xgboost_distribution/distributions/gamma.py
+++ src/xgboost_distribution/distributions/gamma.py
@@ -38,24 +38,20 @@ class Gamma(BaseDistribution):
         digamma_alpha = digamma(alpha)
         grad[:, 0] = - alpha * (beta_prime - digamma_alpha + np.log(y))
         grad[:, 1] = - alpha + beta * y
-        # print(np.average(grad, 0))
-
+    
         hess = np.zeros(shape=(len(y), 2), dtype="float32")
         hess[:, 0] = - grad[:, 0] + alpha * alpha * polygamma(1, alpha)
-        # hess[:, 0, 1] = alpha
-        # hess[:, 1, 0] = alpha
         hess[:, 1] = + beta * y
-        # print(np.average(hess, 0))
         return grad, hess
 
     def loss(self, y, params):
         (alpha,beta) = self.predict(params)
-        # print(-np.average(gamma_dist.logpdf(y, alpha, beta)))
         return "Gamma-NLL", -gamma_dist.logpdf(y, alpha, beta)
 
     def predict(self, params):
         alpha_prime, beta_prime = params[:, 0], params[:, 1]
         return Params(alpha=safe_exp(alpha_prime), beta=safe_exp(beta_prime))
 
-    def starting_params(self, y):
-        return Params(alpha=2, beta=2)
+    def starting_params(self, y: np.ndarray):
+        shape = (y * np.log(y)).mean() - y.mean() * np.log(y).mean()
+        return Params(alpha=np.log(y.mean()/shape), beta=np.log(1/shape))
